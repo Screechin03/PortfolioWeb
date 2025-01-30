8ac6b211bef392222bee04dd1d89c7b66f83d159
Indradeep Mandal <screechin_03@Indradeeps-MacBook-Air.local>
2025-01-30 12:30:06 +0530

    changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <App />
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+
+
+    </BrowserRouter>
+
   </StrictMode>,
 )
