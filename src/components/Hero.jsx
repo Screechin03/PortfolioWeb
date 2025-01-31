5743281277afb761e9c1beb8f65c89dbf0856dd2
Indradeep Mandal <indradeepmandal18@gmail.com>
2025-01-31 14:46:28 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const container = (delay) => ({
 
 const Hero = () => {
     const handleResumeClick = () => {
-        window.open("/INDRADEEP_RES.pdf", "_blank"); // Replace with the actual path to your resume
+        window.open("/INDRADEEP_RES.pdf", "_blank"); 
     };
 
     return (
@@ -46,8 +46,6 @@ const Hero = () => {
                 >
                     I build scalable applications with eye-catching designs and seamless user experience.
                 </motion.h1>
-
-                {/* View Resume Button */}
                 <motion.button
                     variants={container(0.3)}
                     initial="hidden"
@@ -55,14 +53,13 @@ const Hero = () => {
                     onClick={handleResumeClick}
                     className="relative px-6 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group backdrop-blur opacity-80 transition-all duration-300 hover:bg-neutral-900 hover:text-white"
                 >
-                    {/* Border Animations */}
                     <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
                     <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
                     <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
                     <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
                     <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
 
-                    {/* Button Text */}
+    
                     <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease ">
                         Download Resume
                     </span>
